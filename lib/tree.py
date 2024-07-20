ca85fee0470671fe66e509602a6cdc9c8bfa09af
YUERONG HUANG <rowanne.h@hotmail.com>
2024-07-20 16:17:36 +1000

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    node = self.root
+    nodes_to_visit = [node]
+    while len(nodes_to_visit):
+      node = nodes_to_visit.pop()
+      if node['id'] == id:
+        return node
+      nodes_to_visit = node['children']+nodes_to_visit
+    return None 
+
